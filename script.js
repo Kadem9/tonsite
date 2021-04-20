b6aa7bcc6419a3634c06eb7d4235b7c99b4a76fd
Kadem <kadem.garnier@outlook.fr>
2021-04-20 22:18:18 +0200

    première version de tonsite

---
+++ script.js
@@ -0,0 +1,20 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+    mainMenu.style.flexDirection = 'column';
+    mainMenu.style.justifyContent = 'start';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+	  mainMenu.style.display = 'none';
+}
